2df9568ecce5efe72c20cc88a901a0a06fb36b82
anshuman-111 <anshuman11197@gmail.com>
2023-09-07 21:28:39 +1000

    Changed vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   define: {
-    'global' : {},
+    '_global' : ({}),
+    'global' : {}
   },
   plugins: [react()],
+  optimizeDeps: {
+    include: ['axios']
+  }
 })
