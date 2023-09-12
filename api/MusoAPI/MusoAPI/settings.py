e62169505b773eba4dd335ed273e67e34210aefb
anshuman-111 <anshuman11197@gmail.com>
2023-09-12 14:20:16 +1000

    sytnax error

--- api/MusoAPI/MusoAPI/settings.py
+++ api/MusoAPI/MusoAPI/settings.py
@@ -132,7 +132,7 @@ WSGI_APPLICATION = "MusoAPI.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": dj_database_url(os.environ.get('DB_URL'), conn_max_age=600)
+    "default": dj_database_url.parse(os.environ.get('DB_URL'), conn_max_age=600)
 }
 
 
