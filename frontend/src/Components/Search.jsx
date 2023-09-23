f435cf9aae3b620af2aa3e34dbcaf10df6f764ed
anshuman-111 <anshuman11197@gmail.com>
2023-09-23 15:03:59 +1000

    Added UI messages

--- frontend/src/Components/Search.jsx
+++ frontend/src/Components/Search.jsx
@@ -11,7 +11,7 @@ const Search = () => {
 		min: 0,
 		from: "",
 	});
-
+	const [LSFetchMsg, setLSFetchMsg] = useState("");
 	useEffect(() => {
 		const resultsStoredInLS = localStorage.getItem("results");
 		const searchedOnInLS = Number(
@@ -23,6 +23,7 @@ const Search = () => {
 		}
 
 		if (resultsStoredInLS) {
+			setLSFetchMsg("Showing search results for last search");
 			setResults(JSON.parse(resultsStoredInLS));
 		}
 	}, []);
@@ -140,6 +141,7 @@ const Search = () => {
 					Search
 				</button>
 			</form>
+			<h1 className="text-md text-white font-bold">{LSFetchMsg}</h1>
 			<div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
 				<div className="grid mt-10 mx-auto grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 xl:gap-x-8">
 					{results.map((rental) => (
